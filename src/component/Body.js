8ccb2baf137fc86b793cfadbe114dba76008ae33
Kevin DA-RE <dare.kevin21000@gmail.com>
2023-06-21 00:41:23 +0200

    Ajout composants Competence et Experience

---
+++ src/component/Body.js
@@ -0,0 +1,14 @@
+import Experience from './Experience';
+import Competence from './Competence';
+
+function Body(){
+    return (
+        <div>
+<Experience />
+<Competence />
+    </div>
+    )
+    
+}
+
+export default Body;
---
+++ src/component/Competence.js
@@ -0,0 +1,6 @@
+function Competence(){
+    return <h1>Compétences</h1>
+}
+
+
+export default Competence
