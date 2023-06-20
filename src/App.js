35a318ec2d5d99f2b2288895c1620724719d6495
Kevin DA-RE <dare.kevin21000@gmail.com>
2023-06-20 18:30:26 +0200

    init porftolio

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bonjour et bienvenue sur mon portfolio
         </a>
       </header>
     </div>
